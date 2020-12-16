d2061c4e7a4b183b5dbb2f1f29928b8c94163e4c
fabcaz <fcazeils1@protonmail.com>
2020-12-15 19:48:06 -0500

    closes #9

---
+++ src/main/java/com/proj/msorder/services/order/FulfillPartiallyAllocatedOrderService.java
@@ -0,0 +1,40 @@
+package com.proj.msorder.services.order;
+
+
+import com.proj.msorder.domain.KombuchaOrder;
+import com.proj.msorder.domain.KombuchaOrderStatusEnum;
+import com.proj.msorder.repositories.KombuchaOrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A service responsible for ensuring that every order is fully allocated.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FulfillPartiallyAllocatedOrderService {
+
+    private final KombuchaOrderRepository kombuchaOrderRepository;
+    private final KombuchaOrderManager orderManager;
+
+    /**
+     * Periodically retrieves all partially allocated orders and reattempts to allocate inventory for each order.
+     */
+    @Scheduled(fixedRate = 5000)
+    public void fulfillPartiallyAllocatedOrder() {
+        log.debug("Checking for partially allocated orders");
+        List<KombuchaOrder> partiallyAllocatedOrders = kombuchaOrderRepository.findAll().stream()
+                .filter(order -> order.getOrderStatus().equals(KombuchaOrderStatusEnum.PENDING_INVENTORY))
+                .collect(Collectors.toList());
+        log.debug("found "+partiallyAllocatedOrders.size()+" unfulfilled orders");
+        orderManager.processPartiallyAllocatedOrders(partiallyAllocatedOrders);
+
+    }
+
+}
